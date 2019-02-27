00c4d0a0bad5823acc6a958809d44d450bfaac4e
Brion Mario <brion.2015283@iit.ac.lk>
2019-02-27 21:24:14 +0530

    chore(packages): refactor setup.py :package:

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 
 from setuptools import setup, find_packages
 
-setup(name='head-tracker',
+setup(name='headpose',
       version='0.1.0',
       url='https://github.com/brionmario/pnp-head-pose-estimation-poc',
-      description='Head tracking using opencv and dlib',
+      description='Head pose estimation using opencv and dlib',
       author='Brion Silva',
       author_email='brion@apareciumlabs.com',
       packages=find_packages(exclude=('tests', 'docs')),
-      package_data={'head-tracker': ['Readme.md']},
+      package_data={'headpose': ['Readme.md']},
       include_package_data=True,
       license="The MIT License (MIT)"
       )
